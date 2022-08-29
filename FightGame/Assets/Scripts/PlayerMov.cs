88f9c24489812e4f0f627c3ec37bbe1715996ed5
Gezuiz <Lucas.aran1306@gmail.com>
2022-08-29 19:05:44 -0300

    Movimentação avançada pt1
    - dash
    - double jump

--- FightGame/Assets/Scripts/PlayerMov.cs
+++ FightGame/Assets/Scripts/PlayerMov.cs
@@ -5,32 +5,37 @@ using Mirror;
 
 public class PlayerMov : NetworkBehaviour
 {
-
+    #region Variaveis
     public float moveSpeed;
     public float jumpHeight;
     public Rigidbody rigid;
     public bool inAir = false;
-
+    public bool flip;
+    public int doublejump = 2;
+    public int dashpower;
+    public bool dash_Ok = true;
+    #endregion
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if(inAir == false)
-            {
-                rigid.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
-            }
-           
+            jump();
         }
         else if (Input.GetKey(KeyCode.A))
         {
             if(inAir == false)
             {
                 rigid.velocity = new Vector2(-moveSpeed, 0);
+                flip = true;
+                Rotate();
+                
             }
             else
             {
                 rigid.velocity = new Vector2(-moveSpeed, -2);
+                flip = true;
+                Rotate();
             }
 
         }
@@ -40,11 +45,41 @@ public class PlayerMov : NetworkBehaviour
             if (inAir == false)
             {
                 rigid.velocity = new Vector2(moveSpeed, 0);
+                flip = false;
+                Rotate();
+                
             }
             else
             {
                 rigid.velocity = new Vector2(moveSpeed, -2);
+                flip = false;
+                Rotate();
             }
+        }
+        if (inAir == false)
+        {
+            doublejump = 2;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (dash_Ok)
+            {
+                if (flip)
+                {
+                    rigid.AddForce(new Vector3(-dashpower, 0, 0), ForceMode.Impulse);
+                    dash_Ok = false;
+                }
+
+                if (!flip)
+                {
+                    rigid.AddForce(new Vector3(dashpower, 0, 0), ForceMode.Impulse);
+                    dash_Ok = false;
+                }
+            }
+            
+            StartCoroutine(Dash());
+            
         }
 
     }
@@ -68,6 +103,48 @@ public class PlayerMov : NetworkBehaviour
         }
     }
 
+    private void Rotate()
+    {
+        if (flip)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0); // Flipped
+        }
 
+        if (!flip)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void jump()
+    {
+        if (inAir == false)
+        {
+            rigid.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+
+        }
+        if (inAir == true && doublejump > -1)
+        {
+            rigid.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+            doublejump = -1;
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        if (inAir)
+        {
+            yield return new WaitForSeconds(1);
+            dash_Ok = true;
+        }
+
+        if(inAir == false)
+        {
+            yield return new WaitForSeconds(0.5f);
+            dash_Ok = true;
+
+        }
+
+    }
 
 }
